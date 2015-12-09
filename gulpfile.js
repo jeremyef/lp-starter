3e975601386487d1efb598781c918a85554ca5d6
Jeremy Ferrer <jeremy.eferrer@gmail.com>
2015-12-09 15:59:33 +1100

    Added compile sass task

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ var settings = {
     scripts_main: './app/scripts/main.js',
     styles_dir: './app/styles/*',
     styles_main: './app/styles/main.css',
-    sass_dir: './sass/*.scss',
+    sass_dir: './app/styles/sass/*.scss',
     sass_css: './app/styles/css/sass.css',
     bootstrap_dir: './bower_components/bootstrap'
 
@@ -27,11 +27,24 @@ gulp.task('inject_bower',function(cb){
   .pipe(wiredep({directory: settings.bower_dir}))
   .pipe(gulp.dest(settings.app_dir));
 });
-
+// Inject css and js files from app/styles and app/scripts
 gulp.task('inject_main', function(cb){
    return gulp.src(settings.app_index)
   .pipe(plugins.inject(gulp.src([settings.styles_dir + '.css', settings.scripts_dir + '.js'], {read: false}), {relative: true}))
   .pipe(gulp.dest(settings.app_dir));
+});
+
+// Compile SASS files
+gulp.task('compile_sass', function(){
+  return gulp.src(settings.sass_dir)
+  .pipe(plugins.sass.sync().on('error', plugins.sass.logError))
+  .pipe(gulp.dest('./app/styles'));
+});
+
+gulp.task('move_sass', ['compile_sass'], function(){
+  return gulp.src(settings.sass_css)
+  .pipe(plugins.rename('main.css'))
+  .pipe(gulp.dest('./app/styles/'));
 });
 
 gulp.task('inject', function(cb){
