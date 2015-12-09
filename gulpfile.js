b4ed8eff43ae308822ca28f726b396dbe8683da8
Jeremy Ferrer <jeremy.eferrer@gmail.com>
2015-12-09 15:43:52 +1100

    Added bower/main inject tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')({rename: {'gulp-minify-css': 'minifycss'}});
 var wiredep = require('wiredep').stream;
 var bowerFiles = require('main-bower-files');
+var runSequence = require('run-sequence');
 
 var settings = {
     app_dir: './app',
@@ -20,3 +21,19 @@ var settings = {
     bootstrap_dir: './bower_components/bootstrap'
 
 };
+// Inject Bower dependencies to src
+gulp.task('inject_bower',function(cb){
+  return gulp.src(settings.app_index)
+  .pipe(wiredep({directory: settings.bower_dir}))
+  .pipe(gulp.dest(settings.app_dir));
+});
+
+gulp.task('inject_main', function(cb){
+   return gulp.src(settings.app_index)
+  .pipe(plugins.inject(gulp.src([settings.styles_dir + '.css', settings.scripts_dir + '.js'], {read: false}), {relative: true}))
+  .pipe(gulp.dest(settings.app_dir));
+});
+
+gulp.task('inject', function(cb){
+  runSequence('inject_bower', 'inject_main', cb);
+});
